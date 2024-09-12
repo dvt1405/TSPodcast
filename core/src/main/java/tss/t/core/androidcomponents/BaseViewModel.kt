83c045ac060f554bb8bda9436fb8286d4c38ab39
tuandv <tuandv@ahamove.com>
2024-09-12 13:07:58 +0700

    Sample for handle event flow in viewmodel

---
+++ core/src/main/java/tss/t/core/androidcomponents/BaseViewModel.kt
@@ -0,0 +1,56 @@
+package tss.t.core.androidcomponents
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel() : ViewModel() {
+    protected var renderCount: Int = 0
+
+    private val _eventChannel by lazy {
+        Channel<Event>(capacity = Channel.CONFLATED)
+    }
+
+    private val evenHandler by lazy {
+        _eventChannel.consumeAsFlow()
+            .map {
+                ++renderCount
+                handleEvent(it)
+            }
+            .filterNotNull()
+            .onCompletion {
+                Log.e("TuanDv", it?.message, it)
+            }
+    }
+
+    init {
+        evenHandler.launchIn(viewModelScope)
+    }
+
+    fun sendEvent(event: Event, vararg extras: Any) {
+        viewModelScope.launch {
+            _eventChannel.send(event)
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        _eventChannel.close()
+    }
+
+    abstract fun handleEvent(event: Event)
+
+    sealed class Event(val value: Any? = null) {
+        data object Init : Event()
+        data class Click(val id: Int) : Event(id)
+        data class LongClick(val id: Int) : Event(id)
+        data class Refresh(val id: Int = 0) : Event(id)
+    }
+}
--- samples/src/main/java/tss/t/samples/ui/animations/skeleton/ButtonSamples.kt
+++ samples/src/main/java/tss/t/samples/ui/animations/skeleton/ButtonSamples.kt
@@ -1,6 +1,8 @@
 package tss.t.samples.ui.animations.skeleton
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -18,12 +20,19 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Favorite
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import tss.t.core.androidcomponents.BaseViewModel
 import tss.t.samples.ui.animations.skeleton.ui.theme.ComposeLibraryTheme
 import tss.t.sharedlibrary.ui.widget.TSButton
 import tss.t.sharedlibrary.ui.widget.TSButtonDefaults
@@ -33,18 +42,88 @@ import tss.t.sharedlibrary.ui.widget.TSRoundedButton
 import tss.t.sharedlibrary.ui.widget.TSSecondaryButton
 import tss.t.sharedlibrary.ui.widget.TSSecondaryRoundedButton
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 class ButtonSamples : ComponentActivity() {
+    private val viewModel by lazy {
+        ViewModelProvider.create(this)[ButtonViewModel::class]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ComposeLibraryTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TSButtonPreview()
+                    TSButtonPreview(
+                        onClick = {
+                            viewModel.sendEvent(BaseViewModel.Event.Click(it))
+                            viewModel.sendEvent(BaseViewModel.Event.LongClick(it))
+                        }
+                    )
                 }
             }
         }
+        lifecycleScope.launch {
+            viewModel.uiState.collectLatest {
+                Toast.makeText(
+                    this@ButtonSamples,
+                    it.message.plus(" ${it.renderCount}"),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
     }
+}
+
+class ButtonViewModel : BaseViewModel() {
+
+    private val _uiState by lazy {
+        MutableStateFlow(UIState("Init", renderCount = renderCount))
+    }
+
+    val uiState: StateFlow<UIState>
+        get() = _uiState
+
+    override fun handleEvent(event: Event) {
+        when (event) {
+            is Event.Init -> {
+                _uiState.update { it.copy() }
+            }
+
+            is Event.Click -> {
+                _uiState.update {
+                    handleClick(event).copy(renderCount = renderCount) ?: it
+                }
+            }
+
+            else -> {
+
+            }
+        }
+    }
+
+    private fun handleClick(event: Event.Click): UIState {
+        return when (val id = event.id) {
+            0 -> {
+                UIState("HandleClick: $id")
+            }
+
+            1 -> {
+                UIState("HandleClick: $id")
+            }
+
+            2 -> {
+                UIState("HandleClick: $id")
+            }
+
+            else -> {
+                UIState("UnHandleClick for id: $id")
+            }
+        }
+    }
+
+    data class UIState(val message: String, var renderCount: Int = 0)
 }
 
 @Preview(
@@ -52,7 +131,9 @@ class ButtonSamples : ComponentActivity() {
     backgroundColor = 0xFFFFFFFF
 )
 @Composable
-fun TSButtonPreview() {
+fun TSButtonPreview(
+    onClick: (Int) -> Unit = {}
+) {
     ComposeLibraryTheme {
         Column(
             verticalArrangement = Arrangement.spacedBy(16.dp),
@@ -66,69 +147,74 @@ fun TSButtonPreview() {
             TSButton(
                 modifier = Modifier,
                 "Android",
-                onClick = {},
+                onClick = { onClick(1) },
                 contentPadding = TSButtonDefaults.CommonContentPadding
             )
             TSRoundedButton(
                 modifier = Modifier,
                 "Android",
-                onClick = {},
+                onClick = { onClick(2) },
             )
             TSRoundedButton(
                 modifier = Modifier,
                 "Android",
-                onClick = {},
-                rightIcon = Icons.Rounded.Favorite
-            )
+                onClick = { onClick(3) },
+                rightIcon = Icons.Rounded.Favorite,
+
+                )
             TSButton(
                 modifier = Modifier.fillMaxWidth(),
                 "Android",
-                onClick = {},
+                onClick = { onClick(4) },
                 leftIcon = Icons.Rounded.Favorite,
             )
             TSRoundedButton(
                 modifier = Modifier.fillMaxWidth(),
                 "Android",
-                onClick = {},
+                onClick = { onClick(5) },
                 rightIcon = Icons.Rounded.Favorite,
             )
             TSRoundedButton(
                 modifier = Modifier.fillMaxWidth(),
                 "Android",
-                onClick = {},
+                onClick = { onClick(6) },
                 leftIcon = Icons.Rounded.Favorite,
+                enable = false
             )
             TSButton(
                 modifier = Modifier.fillMaxWidth(),
                 "Android",
-                onClick = {},
+                onClick = { onClick(7) },
                 leftIcon = Icons.Rounded.Add,
                 rightIcon = Icons.Rounded.Favorite,
             )
             TSRoundedButton(
                 modifier = Modifier.fillMaxWidth(),
                 "Android",
-                onClick = {},
+                onClick = { onClick(8) },
                 leftIcon = Icons.Rounded.Add,
                 rightIcon = Icons.Rounded.Favorite,
             )
             TSSecondaryRoundedButton(
                 modifier = Modifier.fillMaxWidth(),
                 "Cancel",
-                onClick = {},
+                onClick = { onClick(9) },
             )
             TSSecondaryButton(
                 modifier = Modifier.fillMaxWidth(),
-                title = "Secondary", onClick = { },
+                title = "Secondary",
+                onClick = { onClick(10) },
                 borderSize = 0.dp
             )
             TSOutlineButton(
                 modifier = Modifier.fillMaxWidth(),
-                title = "AndroidOutline", onClick = { },
+                title = "AndroidOutline",
+                onClick = { onClick(11) },
             )
             TSOutlineRoundedButton(
                 modifier = Modifier.fillMaxWidth(),
-                title = "AndroidOutline", onClick = { },
+                title = "AndroidOutline",
+                onClick = { onClick(12) },
             )
 
         }
